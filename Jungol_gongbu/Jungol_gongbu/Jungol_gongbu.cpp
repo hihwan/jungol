f013a45687f87363e310096ef8f919f201506edc
Kimdoohwan <simre1379@gmail.com>
2024-11-05 20:02:44 +0900

    아이스크림 알고리즘

---
+++ Jungol_gongbu/Jungol_gongbu/Jungol_gongbu.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+
+
+int main()
+{
+	int S, A, B, ice = 0;
+	int M = 250;
+
+	scanf("%d %d %d", &S, &A, &B);
+
+	if (S > A)
+	{
+		S -= A;
+		ice = S / B;
+		
+		if (S % A != 0)
+			ice++;
+		printf("%d", ice * M * 100);
+	}
+	else
+	{
+		printf("%d", M);
+	}
+
+	return 0;
+}
